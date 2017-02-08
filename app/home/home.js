19be4297d28d6ea78ecfdf4d08dd6093bb592636
djdolls <hdandroid16@gmail.com>
2017-02-08 21:03:34 +0530

    Sign up and Sign in Ui
    I think changes will required partial for now

--- app/home/home.js
+++ app/home/home.js
@@ -5,14 +5,15 @@ angular.module('EnoticeBoardWebApp.home',['ngRoute','firebase'])
 		templateUrl: 'home/home.html',
 		controller:'HomeCtrl'
 	});
-}]).controller('HomeCtrl', ['$scope', '$firebaseAuth', '$location', 'CommonProp', function($scope, $firebaseAuth, $location, CommonProp){
+}]).controller('HomeCtrl', ['$scope', '$firebaseAuth','$firebaseArray', '$location', 'CommonProp', function($scope, $firebaseAuth,$firebaseArray, $location, CommonProp){
 
 	$scope.username = CommonProp.getUser();
 
 	if($scope.username){
 		$location.path('/welcome');
 	}
-
+   $(".msg").hide();
+   $(".msg1").show();
 	$scope.signIn = function(){
 		var username = $scope.user.email;
 		var password = $scope.user.password;
@@ -37,6 +38,73 @@ angular.module('EnoticeBoardWebApp.home',['ngRoute','firebase'])
 			$scope.errorMessage = error.message;
 		});
 	}
+
+
+
+	//signup
+	$scope.signUp = function(){
+		var username = $scope.user.email;
+		var password = $scope.user.password;
+		var name = $scope.user.username;
+		var val= $scope.user.val;
+		var post= $scope.user.post;
+		var today = new Date();
+		var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+
+        var yyyy = today.getFullYear();
+        if(dd<10){
+	     dd='0'+dd;
+         } 
+         if(mm<10){
+	mm='0'+mm;
+            } 
+        var today = dd+'/'+mm+'/'+yyyy;
+         console.log(name);
+             
+
+		if(username&&password&&name&&val&&post){
+		var auth = $firebaseAuth();
+		auth.$createUserWithEmailAndPassword(username,password).then(function(){
+			console.log("success");
+			var userId = firebase.auth().currentUser.uid;
+			console.log(userId);
+			var ref = firebase.database().ref().child('Users').child(userId);
+			$scope.articles = $firebaseArray(ref);
+			console.log($scope.articles);
+			
+            firebase.database().ref('Users/' + userId).set({
+            	DEST : post,
+            	block : "No",
+            	department : val,
+            	images : "http://s3.amazonaws.com/cdn.roosterteeth.com/default/tb/user_profile_male.jpg",
+            	level : 99,
+            	name : name,
+            	time : "2/10/1995"
+  });
+$location.path('/home');
+          
+
+
+			console.log(userId);
+		}).catch(function(error){
+                $scope.errMsg = true;
+                $scope.errorMessage = error.message;
+		});
+
+
+		
+	}
+	else{
+		alert("Input all values");
+	}
+
+  $(".msg").show();
+   $(".msg1").hide();
+		
+			
+
+};
 
 }])
 
